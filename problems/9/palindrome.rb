0f7e8b964738e1406800a3c27841076db1f02769
Frank Suarez <fsuarez2007@gmail.com>
2025-07-22 17:22:31 -0400

    finished palindrome problem

--- problems/9/palindrome.rb
+++ problems/9/palindrome.rb
@@ -1,30 +1,46 @@
 #!/usr/bin/env ruby
 
+CHECKARGV=true
 
-def isPalindrome(n)
-  
-  # negative numbers are never palindromes
-  return false if (n < 0)
-
-
- 
-  digits = Math.log10(n).to_i + 1
-
-
-
-  while (n > 0)
-    r = n % 10
-    n = n / 10
-
-    puts "#{n} rem. #{r}"
-  end 
-
+# return number of digits in O(1)
+def numberOfDigits(n)
+  return Math.log10(n).to_i + 1
 end
 
+def oppositeIndex(index, length)
+  return length - index - 1
+end
 
-isPalindrome(10501)
+# returns the digit at position in O(1)
+# zero index
+def getDigit(n, pos)
+  digits = numberOfDigits(n)
 
+  return (n / (10 ** (digits-pos-1))) % 10
+end
 
+def isPalindrome(n)
+  # negative numbers are never palindromes
+  return false if (n < 0)
+ 
+  digits = numberOfDigits(n)  
 
+  # go from 0 to center
+  # because number of digits is known,
+  # we can match it with the corresponding digit
 
+  for i in (0..digits-1)
+    d1 = getDigit(n,i)
+    d2 = getDigit(n,oppositeIndex(i,digits))
+    return false if (d1 != d2)
+    # puts "#{i}: #{d1} #{d2}"
+  end
+  return true
+end
+
+if CHECKARGV then
+  for n in ARGV
+    puts "#{n}: #{isPalindrome(n.to_i)}"
+  end
+end
 
---
+++ problems/9/test.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+# problem verification
+
+
+require 'palindrome'
+
+
+
+
