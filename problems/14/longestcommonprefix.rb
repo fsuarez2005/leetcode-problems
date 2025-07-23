f6af68e27aa6094f6d0bd3984598bb9de1ca7ae7
Frank Suarez <fsuarez2007@gmail.com>
2025-07-23 09:30:23 -0400

    problem is in solved state. need to clean up.

--- problems/14/longestcommonprefix.rb
+++ problems/14/longestcommonprefix.rb
@@ -6,18 +6,41 @@
 
 def longestcommonprefix(strArr)
   currentIndex = 0
-
+  matchingChars = []
   while (true)
+    lastChar = nil
+
+
     for s in strArr
       puts s[currentIndex]
+      if s[currentIndex] == nil
+        # no more chars for current string
+        return matchingChars
+      end
 
+
+
+      if lastChar == nil
+        lastChar = s[currentIndex]
+      elsif lastChar != s[currentIndex]
+        # chars do not match
+        return matchingChars
+      else
+
+      end
+
+
+        
 
     end
+    matchingChars << strArr[0][currentIndex]
+    currentIndex = currentIndex + 1
+
   end
 
 
 end
 
 
-longestcommonprefix(["frank","fry","friend"])
-
+m = longestcommonprefix(["frank","fry","friend"])
+p m
