d9effe4f38a606b6009b8aaa9f7c72b53b1ffce9
Frank Suarez <fsuarez2007@gmail.com>
2025-07-23 09:22:04 -0400

    starting problem 14

---
+++ problems/14/longestcommonprefix.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+
+
+
+
+def longestcommonprefix(strArr)
+  currentIndex = 0
+
+  while (true)
+    for s in strArr
+      puts s[currentIndex]
+
+
+    end
+  end
+
+
+end
+
+
+longestcommonprefix(["frank","fry","friend"])
+
